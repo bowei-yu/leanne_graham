6fcb3c692cb0eb491dce94559b3832fec74455d1
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2021-02-05 14:38:59 +0800

    Add basic home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+// Components
+import Navbar from './components/Navbar';
+
+//Pages
+import home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar/>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home}/>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
